57279495192c5f3244737de106ee835a4550cf3c
GaelLouage <Gael.Louage@hotmail.com>
2022-08-15 12:49:39 +0200

    jenkins

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string ex = "Hello World!";
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                Jenkins_test.Program.Main();
+                //Tester a = new Tester();
+                //a.a = 1;
+                //a.b = 2;
+                var result = sw.ToString().Trim();
+                Assert.AreEqual(ex, result);
+            }
+        }
+    }
+}
